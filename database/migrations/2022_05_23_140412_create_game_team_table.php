c071ad96d169a580e45363a9823ff14975be1e05
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2024-04-02 01:54:18 +0200

    restucture

--- database/migrations/2022_05_23_140412_create_game_team_table.php
+++ database/migrations/2022_05_23_140412_create_game_team_table.php
@@ -1,13 +1,13 @@
 <?php
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 class CreateGameTeamTable extends Migration
 {
     public function up(){
         Schema::create('game_team', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('game_id')->constrained('games')->onDelete('cascade');
+            $table->foreignId('game_id');
             $table->string('team_title');
             $table->integer('capacity');
         });
